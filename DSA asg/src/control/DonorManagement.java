52bab7a07343ab99a60c3992dba61ef1efb17ccf
lxy0626 <xylai224@gmail.com>
2024-08-30 18:11:57 +0800

    comment the line

--- DSA asg/src/control/DonorManagement.java
+++ DSA asg/src/control/DonorManagement.java
@@ -20,48 +20,48 @@ public class DonorManagement {
 
     DonorManagementUI ui = new DonorManagementUI();
     LinkedHashMapInterface<String, Donor> donorMap;
-    private final DonationManagement donationManagement;
+//    private final DonationManagement donationManagement;
     private final DonorDataSeeder dataSeeder;
 
     public DonorManagement() {
         donorMap = new LinkedHashMap<>();
-        this.donationManagement = donationManagement;
+//        this.donationManagement = donationManagement;
         dataSeeder = new DonorDataSeeder();
-        dummyData();
+//        dummyData();
     }
 
-    private void dummyData() {
-        List<DonationItem> donationItems = new List<>();
-//        String[] expList = new String[]{"Shirt", "Food", "Items"};
-
-        // Create some DonationItem objects
-        DonationItem item1 = new DonationItem("ITEM-001", "Food", 10, "kfc");
-        DonationItem item2 = new DonationItem("ITEM-002", "Clothes", 5, "h&m shirt");
-        DonationItem item3 = new DonationItem("ITEM-003", "Books", 7, "harry porter");
-
-        // Add items to the list
-        donationItems.add(item1);
-        donationItems.add(item2);
-        donationItems.add(item3);
-        // Create donations
-        Donation donation1 = new Donation("DON-001", "DNR-001", donationItems);
-        Donation donation2 = new Donation("DON-002", "DNR-002", donationItems);
-        Donation donation3 = new Donation("DON-003", "DNR-003", donationItems);
-        Donation donation4 = new Donation("DON-004", "DNR-003", donationItems);
-
-        Donor dp = new Donor("Deadpool", "government", "0123456789");
-        Donor wv = new Donor("Wolverine", "private", "0123456789");
-        Donor jf = new Donor("Johnny Flame", "public", "0123456789");
-
-        donorMap.put(dp.getDonorId(), dp);
-        donorMap.put(wv.getDonorId(), wv);
-        donorMap.put(jf.getDonorId(), jf);
-
-        dp.addDonation(donation1);
-        dp.addDonation(donation2);
-        wv.addDonation(donation3);
-        jf.addDonation(donation4);
-    }
+//    private void dummyData() {
+//        List<DonationItem> donationItems = new List<>();
+////        String[] expList = new String[]{"Shirt", "Food", "Items"};
+//
+//        // Create some DonationItem objects
+//        DonationItem item1 = new DonationItem("ITEM-001", "Food", 10, "kfc");
+//        DonationItem item2 = new DonationItem("ITEM-002", "Clothes", 5, "h&m shirt");
+//        DonationItem item3 = new DonationItem("ITEM-003", "Books", 7, "harry porter");
+//
+//        // Add items to the list
+//        donationItems.add(item1);
+//        donationItems.add(item2);
+//        donationItems.add(item3);
+//        // Create donations
+//        Donation donation1 = new Donation("DON-001", "DNR-001", donationItems);
+//        Donation donation2 = new Donation("DON-002", "DNR-002", donationItems);
+//        Donation donation3 = new Donation("DON-003", "DNR-003", donationItems);
+//        Donation donation4 = new Donation("DON-004", "DNR-003", donationItems);
+//
+//        Donor dp = new Donor("Deadpool", "government", "0123456789");
+//        Donor wv = new Donor("Wolverine", "private", "0123456789");
+//        Donor jf = new Donor("Johnny Flame", "public", "0123456789");
+//
+//        donorMap.put(dp.getDonorId(), dp);
+//        donorMap.put(wv.getDonorId(), wv);
+//        donorMap.put(jf.getDonorId(), jf);
+//
+//        dp.addDonation(donation1);
+//        dp.addDonation(donation2);
+//        wv.addDonation(donation3);
+//        jf.addDonation(donation4);
+//    }
 
     public void runSystem() {
         boolean running = true;
